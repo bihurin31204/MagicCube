4bea6aa045ab2a652f984060f7eb7c353de90bd2
leaxz <thaliafahira@gmail.com>
2024-11-09 22:06:04 +0700

    fix code

--- src/State.py
+++ src/State.py
@@ -1,49 +1,61 @@
+import numpy as np
+
 class State():
-    def __init__(self, state, goal_value=109):
-        self.state = state
-        self.goal_value = goal_value
+    def __init__(self, state=None):
+        if state is not None:
+            self.state = state.reshape(5, 5, 5)
+        else:
+            self.state = self.generate_state()
+        self.value = self.evaluate()
+
+    def generate_state(self):
+        state = np.random.choice(np.arange(1, 126), size=125, replace=False).reshape((5, 5, 5))
+        return state
 
     def evaluate(self):
-        count = 0
-        target_sum = 315
-        # Hitung maksimal jumlah baris/kolom/diagonal yang memiliki sum yang sama
-        #return max()
-        #checking baris
-        for layer in self.state:
-            for row in layer:
-                if sum(row) == target_sum:
-                    count += 1
-               
-        #checking kolom     
-        for i in range(5):
-            for layer in self.state:
-                if sum(layer[j][i] for j in range(5)) == target_sum:
-                    count += 1
-        
-        #checking tiang 
-        for i in range(5):
-            for j in range(5):
-                if sum(self.state[k][i][j] for k in range(5)) == target_sum:
-                    count += 1
-        
-        #checking diagonal layer
-        for layer in self.state:
-            if sum(layer[i][i] for i in range(5)) == target_sum:
-                count += 1
-            if sum(layer[i][4-i] for i in range(5)) == target_sum:
-                count += 1
-        
-        #checking diagonal ruang
-        if sum(self.state[i][i][i] for i in range(5)) == target_sum:
-            count += 1
-        if sum(self.state[i][i][4-i] for i in range(5)) == target_sum:
-            count += 1
-        if sum(self.state[i][4-i][i] for i in range(5)) == target_sum:
-            count += 1
-        if sum(self.state[4-i][i][i] for i in range(5)) == target_sum:
-            count += 1
-        
-        return count
+        # pillars (25)
+        pillar_sum = np.sum(self.state, axis=0).flatten()
+
+        # rows (25)
+        row_sum = np.sum(self.state, axis=2).flatten()
+
+        # collumns (25)
+        column_sum = np.sum(self.state, axis=1).flatten()
+
+        # space diagonals (4)
+        space_diagonals = []
+        space_diagonals.append([self.state[i, i, i] for i in range(5)])
+        space_diagonals.append([self.state[i, i, 4 - i] for i in range(5)])
+        space_diagonals.append([self.state[i, 4 - i, i] for i in range(5)])
+        space_diagonals.append([self.state[4 - i, i, i] for i in range(5)])
+        space_diagonal_sum = np.sum(space_diagonals, axis=1)
+
+        # diagonals (30)
+        diagonals = []
+        for n in range(5):
+            # plane xy
+            diagonals.append([self.state[i, i, n] for i in range(5)])
+            diagonals.append([self.state[i, 4-i, n] for i in range(5)])
+            # plane xz
+            diagonals.append([self.state[i, n, i] for i in range(5)])
+            diagonals.append([self.state[i, n, 4-i] for i in range(5)])
+            # plane yz
+            diagonals.append([self.state[n, i, i] for i in range(5)])
+            diagonals.append([self.state[n, i, 4-i] for i in range(5)])
+        diagonal_sum = np.sum(diagonals, axis=1)
+
+        # collect all features
+        feature_sum = np.concatenate((pillar_sum, row_sum, column_sum, space_diagonal_sum, diagonal_sum))
+
+        # calculate state value
+        self.value = 0
+        for x in feature_sum:
+            self.value -= abs(x-315)
+
+        return self.value
 
     def is_goal_state(self):
-        return self.evaluate() == self.goal_value
+        return self.value == 0
+    
+    def __str__(self):
+        return np.array2string(self.state, separator=' ')
