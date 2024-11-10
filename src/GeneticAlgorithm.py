a6c79c7884d1c577e2d9b0b54eedcc7101bf6d3e
mhmmdhkm <mhmmd.hkm25@gmail.com>
2024-11-10 11:42:02 +0700

    genetic algortihm & simulated annealing

--- src/GeneticAlgorithm.py
+++ src/GeneticAlgorithm.py
@@ -1,29 +1,129 @@
-class GeneticAlgorithm(Algorithm):
-    def __init__(self, population_size, mutation_rate, crossover_rate):
+import numpy as np
+import random
+from State import State
+
+GENES = 125
+
+class GeneticAlgorithm():
+    def __init__(self, population_size, max_iteration, mutation_rate):
         self.population_size = population_size
+        self.max_iteration = max_iteration
         self.mutation_rate = mutation_rate
-        self.crossover_rate = crossover_rate
-        self.population = self.initialize_population()
+        self.population = np.array([np.random.choice(np.arange(1, 126), size=125, replace=False) for _ in range(self.population_size)])
+    
+    def random_selection(self):
+        fitnesses = np.array([self.fitness(individual) for individual in self.population])
+        probabilities = fitnesses / fitnesses.sum()
+        parent_index = np.random.choice(np.arange(self.population_size), p=probabilities)
+        parent = self.population[parent_index]
+        return parent
+        
+    # Crossover function (single-point crossover)
+    def reproduce(self, parent1, parent2):
+        point = random.randint(1, GENES-1)
+        child = np.concatenate((parent1[:point], parent2[point:]))
+        return child
+    
+    def mutate(self, individual):
+        point = random.randrange(GENES)
+        gene = random.randint(1, GENES)
+        individual[point] = gene
+        return individual
+    
+    def get_feature_sum(self, individual):
+        state = np.copy(individual).reshape(5, 5, 5)
 
-    def initialize_population():
-        return np.array([1, 2, 3, ..., 125])
+        # pillars (25)
+        pillar_sum = np.sum(state, axis=0).flatten()
 
-    def search (self):
-        while not self.is_goal_state_in_population () :
-            parents = self.select_parents ()
+        # rows (25)
+        row_sum = np.sum(state, axis=2).flatten()
+
+        # collumns (25)
+        column_sum = np.sum(state, axis=1).flatten()
+
+        # space diagonals (4)
+        space_diagonals = []
+        space_diagonals.append([state[i, i, i] for i in range(5)])
+        space_diagonals.append([state[i, i, 4 - i] for i in range(5)])
+        space_diagonals.append([state[i, 4 - i, i] for i in range(5)])
+        space_diagonals.append([state[4 - i, i, i] for i in range(5)])
+        space_diagonal_sum = np.sum(space_diagonals, axis=1)
+
+        # diagonals (30)
+        diagonals = []
+        for n in range(5):
+            # plane xy
+            diagonals.append([state[i, i, n] for i in range(5)])
+            diagonals.append([state[i, 4-i, n] for i in range(5)])
+            # plane xz
+            diagonals.append([state[i, n, i] for i in range(5)])
+            diagonals.append([state[i, n, 4-i] for i in range(5)])
+            # plane yz
+            diagonals.append([state[n, i, i] for i in range(5)])
+            diagonals.append([state[n, i, 4-i] for i in range(5)])
+        diagonal_sum = np.sum(diagonals, axis=1)
+
+        # collect all features
+        feature_sum = np.concatenate((pillar_sum, row_sum, column_sum, space_diagonal_sum, diagonal_sum))
+
+        return feature_sum
+    
+    def fitness(self, individual):
+        feature_sum = self.get_feature_sum(individual)
+
+        # calculate fitness
+        value = 0
+        for x in feature_sum:
+            value -= abs(x-315)
+
+        return 10000 + value
+    
+    def evaluate(self, individual):
+        feature_sum = self.get_feature_sum(individual)
+        
+        # calculate state value
+        value = 0
+        for x in feature_sum:
+            value -= abs(x-315)
+
+        return value
+    
+    def is_goal_state_in_population(self):
+        values = np.array([self.evaluate(individual) for individual in self.population])
+        return np.any(values == 0)
+    
+    def get_best_individual(self):
+        values = np.array([self.evaluate(individual) for individual in self.population])
+        best_index = np.argmax(values)
+        best_individual = self.population[best_index]
+        return best_individual
+    
+    def search(self):
+        iteration = 1
+        values = [self.evaluate(self.get_best_individual())]
+        while not self.is_goal_state_in_population() and iteration <= self.max_iteration:
+            print(f'iteration: {iteration}')
             offspring = []
-            for i in range(0, len(parents), 2):
-                parent1, parent2 = parents [i, parents [i + 1]
-                if (random_probability) < self.crossover_rate:
-                    child1, child2 = self.crossover (parent1, parent2) # Crossover antara dua parent
-                else:
-                    child1, child2 = parent1, parent2
-                offspring.append(child1)
-                offspring.append(child2)
-            for child in offspring:
-                if random_probability() < self.mutation_rate:
-                    self.mutate(child) # Mutasi pada child
+            for _ in range(self.population_size):
+                # Selection
+                parent1 = self.random_selection()
+                parent2 = self.random_selection()
 
-            self.population = self.select_survivors (offspring) # Seleksi generasi berikutnya
-        return self.get_best_individual()
+                # Crossover / Reproduce
+                child = self.reproduce(parent1, parent2) # Crossover antara dua parent
 
+                # Mutation
+                random_probability = random.random()
+                if random_probability > self.mutation_rate:
+                    child = self.mutate(child)
+                
+                offspring.append(child)
+            self.population = offspring
+            best_individual = self.get_best_individual()
+            best_individual_value = self.evaluate(best_individual)
+            print(f'best individual value: {best_individual_value}')
+            values.append(best_individual_value)
+            iteration += 1
+        final_state = State(best_individual)
+        return final_state, values
--- src/SimulatedAnnealing.py
+++ src/SimulatedAnnealing.py
@@ -1,37 +1,56 @@
-import State
+from State import State
 import random
-import math
 import numpy as np
 
 class SimulatedAnnealing():
-    def _init_(self, initial_temperature, cooling_rate):
+    def __init__(self, initial_temperature, cooling_rate):
+        self.current_state = State()
         self.temperature = initial_temperature
         self.cooling_rate = cooling_rate
-        # TODO
-        # masukin elemen state dan nilai goal_value
-        self.current_state = State(np.array([]), 0)
 
-    def generate_neighbors(self):
-        # TODO
-        # cari cara untuk generate neighbors
-        return []
+    def generate_successors(self):
+        indices = [(i, j, k) for i in range(5) for j in range(5) for k in range(5)]
+        successors = []
+        for i in range(len(indices)):
+            for j in range(i + 1, len(indices)):  # Pastikan tidak menukar elemen yang sama
+                index_1 = indices[i]
+                index_2 = indices[j]
+                successor = State()
+                successor.state = np.copy(self.current_state.state)
+                successor.state[index_1], successor.state[index_2] = self.current_state.state[index_2], self.current_state.state[index_1]
+                successor.value = successor.evaluate()
+                successors.append(successor)
+        successors = np.array(successors)
+        return successors
     
-    def select_random_neighbor(self, neighbors):
-        return random.choice(neighbors)
+    def select_random_successor(self, successors):
+        random_index = random.randrange(len(successors))
+        neighbor = successors[random_index]
+        return neighbor
 
     def search(self):
-        while not self.current_state.is_goal_state() and self. temperature > 0:
-            neighbors = self.generate_neighbors()
-            random_neighbor = self.select_random_neighbor(neighbors) # Pilih tetangga acak
-            delta = random_neighbor.evaluate_state() - self.current_state.evaluate_state()
-            if delta > O:
-                self.current_state = random_neighbor # Pindah jika lebih baik
-        else:
-            random_probability = random.uniform(0, 1)
-            if random_probability < math.exp(delta / self.temperature):
-                self.current_state = random_neighbor # Pindah dengan probabilitas
+        print(f'initial state value: {self.current_state.value}')
+        local_optima = 0
+        probs = []
+        values = [self.current_state.value]
+        while not self.current_state.is_goal_state() and self.temperature > 0:
+            successors = self.generate_successors()
+            neighbor = self.select_random_successor(successors) # Pilih tetangga acak
+            delta =  neighbor.value - self.current_state.value
+            if delta > 0:
+                self.current_state = neighbor # Pindah jika lebih baik
+                print(f'current state value: {neighbor.value}')
+            else:
+                random_probability = random.random()
+                if random_probability < np.exp(delta / self.temperature):
+                    self.current_state = neighbor # Pindah dengan probabilitas
+                    print(f'current state value: {neighbor.value}')
+                    local_optima += 1
+            probs.append(np.exp(delta / self.temperature))
             self.temperature -= self.cooling_rate # Kurangi temperatur
-        return self.current_state
-
-sa = SimulatedAnnealing(200, 1)
-print(type(sa))
+            print(f'current temperature: {round(self.temperature, 1)}')
+            values.append(self.current_state.value)
+        print('terminate')
+        print(f'final state value: {self.current_state.value}')
+        print(f'local optima freq: {local_optima}')
+        return self.current_state, values, probs
